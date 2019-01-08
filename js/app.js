17a03299b26075794731d949a2b8dbe849f02c62
Rohit Parida <rohitkparida@gmail.com>
2019-01-08 19:14:06 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,123 +1,136 @@
-// Clear - Button
-$('#clear').click(function() {
-    $("#primary").val('');
-    $("#secondary").val('');
-});
-
-
-// var display_input = document.getElementById("display_input"),
-//     display_output = document.getElementById("display_output"),
-//     display_animation = document.getElementById("display_animation"),
-//     display_clear = document.getElementById("display_clear");
-//
-//
-// function calculate() {
-//     "use strict";
-//
-//     var value = display_input.innerHTML;
-//     value = value.split("Ã·").join("/");
-//     value = value.split("Ã—").join("*");
-//     value = value.split("â€“").join("-");
-//
-//     try {
-//         value = eval(value);
-//         display_output.innerHTML = value;
-//     } catch (e) {
-//         display_output.innerHTML = "Â¯\\_(ãƒ„)_/Â¯";
-//     }
-//
-//     display_input.scrollLeft = display_input.scrollWidth - display_input.clientWidth;
-//     display_output.scrollLeft = display_output.scrollWidth - display_output.clientWidth;
-// }
-
-
-
-$(".input").click(function() {
-    try {
-        var txt = $(this).val();
-        $("#primary").val($("#primary").val() + txt);
-        var primary = $("#primary").val();
-        console.log(primary);
-        primary = primary.split("\xF7").join("/").split("\xD7").join("*").split("u2212").join("-").split("π").join("22/7");
-        // replace("\xF7", "/1").replace("\xD7", "*1").replace("+", "+0").replace("\u2212", "-0").replace("π", "22/7");
-        console.log(primary);
-        ans = eval(primary.toString());
-        $("#secondary").val(ans);
-        console.log(ans);
-
-    } catch (e) {
-        $("#secondary").val('IDK');
+(function localFileVideoPlayer() {
+    'use strict'
+    var URL = window.URL || window.webkitURL
+    var displayMessage = function(message, isError) {
+        var element = document.querySelector('#message')
+        element.innerHTML = message
+        element.className = isError ? 'error' : 'info'
     }
+    var playSelectedFile = function(event) {
+        var file = this.files[0]
+        var type = file.type
+        var videoNode = document.querySelector('video')
+        var canPlay = videoNode.canPlayType(type)
+        if (canPlay === '') canPlay = 'no'
+        var message = 'Can play type "' + type + '": ' + canPlay
+        var isError = canPlay === 'no'
+        displayMessage(message, isError)
 
-});
+        if (isError) {
+            return
+        }
 
-$("#primary").keyup(function() {
-    // var primary = $("#primary").val();
-    // var secondary = $("#secondary").val();
-    // var txt = $(this).val();
-    // $("#primary").val(primary);
-    // var primary = $("#primary").val();
-    // console.log(primary);
-    // var fix = primary.replace("/", "b");
-    // console.log(fix);
-    // $("#primary").val(primary);
-    // var ans = eval(primary);
-    $("#secondary").val(ans);
-});
+        var fileURL = URL.createObjectURL(file)
+        videoNode.src = fileURL
+    }
+    var inputNode = document.querySelector('.receiver')
+    inputNode.addEventListener('change', playSelectedFile, false)
+})()
 
 
-$('#result').click(function() {
-    var secondary = $("#secondary").val();
-    $("#primary").val(secondary);
-    $("#secondary").val('');
 
-});
-$("#inputs").on('scroll load', function() {
-if ($('#content').scrollTop() >= $("#content").prop("scrollHeight")) {
-  console.log("hey");
+
+document.querySelector('video').playbackRate = 3.0;
+
+
+// STATUS-----------
+
+// var element = $('#element');
+//
+// element.waiting(function() {
+//     console.log(this);
+//     if (this.paused) {
+//         $(".status").addClass("playing");
+//     } else {
+//         $(".status").addClass("error");
+//     }
+// });
+
+// Disable_sendBtn-----------------------------
+function success() {
+    if (document.getElementById("newURL").value === "") {
+        document.getElementById('send').disabled = true;
+    } else {
+        document.getElementById('send').disabled = false;
+    }
 }
+
+// Pause control
+// $("video").click(function() {
+//   //console.log(this);
+//   if (this.paused) {
+//     this.play();
+//   } else {
+//     this.pause();
+//   }
+// });
+
+// ____________________________PLAYBACK_GESTURES
+$(document).ready(function() {
+
+    var element = $('#element');
+    var debug = $('#debug');
+
+    element.touch = {}
+
+    // element.click(function() {
+    //     console.log(this);
+    //     if (this.paused) {
+    //         this.play();
+    //     } else {
+    //         this.pause();
+    //     }
+    // });
+    element.get(0).ontouchstart = function(e) {
+        // Store start position
+        element.touch.x = e.touches[0].clientX;
+    };
+
+    element.get(0).ontouchmove = function(e) {
+        // only deal with one finger
+        if (e.touches.length == 1 && !element.is(":animated")) {
+
+            // Get delta
+            var deltaX = element.touch.x - e.touches[0].clientX;
+
+            // Move foward
+            if (deltaX > 20) {
+                this.currentTime -= .5;
+            }
+            // Move backward
+            else if (deltaX < -20) {
+                this.currentTime += .5;
+            }
+        }
+    };
+
 });
 
-function scrolld() {
-    document.getElementById("inputs").scrollBy(0, document.documentElement.clientHeight);
-    console.log("wurk");
-}
 
-
-$("#inputs").on('scroll load', function() {
-    var winScroll = $("#inputs").scrollTop();
-    var height = $("#inputs").prop("scrollHeight");
-    var degree = (winScroll / height) * 360;
-    console.log(degree);
-    document.getElementById("chevron").style.transform = "rotateX("+ degree +"deg)";
-});
-
-
-$("#content").scrollTop($("#content").prop("scrollHeight"));
-// var fix = primary.replace(/, "b");
-// console.log(fix);
-
-
-// $('#clear').click(function() {
-//     var primary = $("#primary").val();
-//     var primaryl = $("#primary").length;
-//     console.log(primaryl);
-//     $("#primary").val(primary.substring(0, primaryl - 1));
+//DIM Control_______________________________
+// $('#contrast').on('input', function() {
+//     $('#element').css('opacity', $(this).val());
 // });
 
 
+// ___________DEFAULT_VOLUME
+$("video").prop("volume", 0.6);
 
-// (localStorage.getItem('theme') === 'dark') {
-//
-//     $(".theme").prop("disabled", false);
-//     $('meta[name=theme-color]').remove();
-//     $('head').append('<meta name="theme-color" content="#212121">');
-//
-// }
-//
-// var isDisabled = $(".theme").prop('disabled');
-// if (isDisabled) {
-//     localStorage.setItem('theme', 'light');
-// } else {
-//     localStorage.setItem('theme', 'dark');
-// }
+
+//URL______________________________
+function changeURL() {
+    var newURL = document.getElementById("newURL").value;
+    document.getElementById("element").src = newURL;
+}
+
+
+// Registering ServiceWorker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
